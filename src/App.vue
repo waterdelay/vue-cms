451feaa735dc72083eeeaf0fd82b7d4416a69954
waterdelay <397249058@qq.com>
2018-12-31 15:27:59 +0800

    修改tabbar

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,40 @@
 <template>
   <div id="app">
+    <!-- header 头部 -->
+    <mt-header fixed title="Vue CMS"></mt-header>
+    <!-- <router-view></router-view> -->
+    <h1>123456</h1>
+    <!-- 底部 -->
+    <nav class="mui-bar mui-bar-tab">
+      <a class="mui-tab-item mui-active" href="#tabbar">
+        <span class="mui-icon mui-icon-home"></span>
+        <span class="mui-tab-label">首页</span>
+      </a>
+      <a class="mui-tab-item" href="#tabbar-with-chat">
+        <span class="mui-icon mui-icon-contact">
+          
+        </span>
+        <span class="mui-tab-label">会员</span>
+      </a>
+      <a class="mui-tab-item" href="#tabbar-with-contact">
+        <span class="mui-icon mui-icon-extra mui-icon-extra-cart">
+          <span class="mui-badge">0</span>
+        </span>
+        <span class="mui-tab-label">购物车</span>
+      </a>
+      <a class="mui-tab-item" href="#tabbar-with-map">
+        <span class="mui-icon mui-icon-search"></span>
+        
+        <span class="mui-tab-label">搜素</span>
+      </a>
+    </nav>
   </div>
 </template>
 
 <script>
-export default {
-  name: "App"
-};
+export default {};
 </script>
 
-<style>
+<style lang="less">
 
 </style>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,25 @@
+//入口文件
 import Vue from 'vue'
-import App from './App'
+
+// 1.1 导入路由的包
 import router from './router'
-// 引用MintUI组件
+// 1.2 安装路由
+Vue.use(MintUI)
+// 引用MintUI组件 全局
 import MintUI from 'mint-ui'
 import 'mint-ui/lib/style.css'
-Vue.use(MintUI)
+import { Header } from 'mint-ui';
+
+//导入mui
+import "./lib/mui/css/mui.min.css"
+//导入扩展图标样式
+import "./lib/mui/css/icons-extra.css"
+
+
+Vue.component(Header.name, Header);
 Vue.config.productionTip = false
 /* eslint-disable no-new */
+import App from './App'
 new Vue({
   el: '#app',
   router,
